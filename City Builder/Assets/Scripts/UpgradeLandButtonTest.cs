9faddcd4a6e7044fdf1b354b845516d4ba05b868
Prostokishyn <dprostokishyn@rcit.ukr.education>
2023-11-28 21:54:20 +0200

    Shop updated
    Added other buildings

--- City Builder/Assets/Scripts/UpgradeLandButtonTest.cs
+++ City Builder/Assets/Scripts/UpgradeLandButtonTest.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using static UnityEditor.Timeline.TimelinePlaybackControls;
@@ -8,51 +9,18 @@ using static UnityEngine.EventSystems.EventTrigger;
 
 public class UpgradeLandButtonTest : MonoBehaviour
 {
-    public GameObject grid2;
-    public Tile[] tiles2;
-
     public GameObject land2;
-
     public GameObject land1;
 
-    private Building buildingToPlace;
-    public GameObject grid;
+    public GameObject upgradeButton;
 
     public void OnMouseDown()
     {
-        land1.SetActive(!land1.activeSelf);
+        land1.SetActive(false);
 
-        land2.SetActive(!land2.activeSelf);
+        land2.SetActive(true);
 
-    }
+        Destroy(upgradeButton);
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0) && buildingToPlace != null)
-        {
-
-            Tile nearestTile = null;
-            float nearestDistance = float.MaxValue;
-            foreach (Tile tile in tiles2)
-            {
-                float dist = Vector2.Distance(tile.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
-                if (dist < nearestDistance)
-                {
-                    nearestDistance = dist;
-                    nearestTile = tile;
-                }
-            }
-            if (nearestTile.isOccupied == false)
-            {
-
-                Instantiate(buildingToPlace, nearestTile.transform.position, Quaternion.identity);
-                buildingToPlace = null;
-                nearestTile.isOccupied = true;
-                grid.SetActive(false);
-
-                //customCursor.gameObject.SetActive(false);
-                Cursor.visible = true;
-            }
-        }
     }
 }
