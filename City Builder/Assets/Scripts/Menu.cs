1721f93d7e44b10551086868ef0780e656f306d0
Prostokishyn <dprostokishyn@rcit.ukr.education>
2023-12-07 14:43:34 +0200

    Added menu of levels
    Three buttons. there is only one level

--- City Builder/Assets/Scripts/Menu.cs
+++ City Builder/Assets/Scripts/Menu.cs
@@ -6,13 +6,32 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    public void Play()
+    public GameObject menu;
+    public GameObject LevelsMenu;
+
+    public void OpenCloseLevels()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        menu.SetActive(!menu.activeSelf);
+        LevelsMenu.SetActive(!LevelsMenu.activeSelf);
     }
 
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void PlayLvl1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayLvl2()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayLvl3()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
