d0350f54c8fd8c527fd2a0f0432b469a0dd9788f
Prostokishyn <dprostokishyn@rcit.ukr.education>
2023-12-09 17:58:20 +0200

    Test experience system
    Added bar and script. 1 click +50 exp, max lvl - 5

---
+++ City Builder/Assets/Scripts/ExpSystem.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExpSystem : MonoBehaviour
+{
+    public GameObject UpgradeLandButton;
+
+    [Header("Experience")]
+    [SerializeField] AnimationCurve experienceCurve;
+
+    int currentLevel, totalExperience;
+    int previousLevelsExperience, nextLevelsExperience;
+
+    int maxLevel = 5;
+
+    [Header("Interface")]
+    [SerializeField] TextMeshProUGUI levelText;
+    [SerializeField] TextMeshProUGUI experienceText;
+    [SerializeField] Image experienceFill;
+
+    void Start()
+    {
+        UpdateLevel();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            AddExperience(50);
+        }
+    }
+
+    public void AddExperience(int amount)
+    {
+        totalExperience += amount;
+        CheckForLevelUp();
+        UpdateInterface();
+    }
+
+    void CheckForLevelUp()
+    {
+        if (totalExperience >= nextLevelsExperience)
+        {
+            currentLevel++;
+            UpdateLevel();
+
+            // Start level up sequence... Possibly vfx?
+        }
+    }
+
+    void UpdateLevel()
+    {
+        previousLevelsExperience = (int)experienceCurve.Evaluate(currentLevel);
+        nextLevelsExperience = (int)experienceCurve.Evaluate(currentLevel + 1);
+        UpdateInterface();
+    }
+
+    void UpdateInterface()
+    {
+        int start = totalExperience - previousLevelsExperience;
+        int end = nextLevelsExperience - previousLevelsExperience;
+
+        if (currentLevel < maxLevel)
+        {
+            levelText.text = currentLevel.ToString();
+            experienceText.text = start + " exp / " + end + " exp";
+            experienceFill.fillAmount = (float)start / (float)end;
+        }
+        else
+        {
+            levelText.text = currentLevel.ToString();
+            experienceFill.fillAmount =1f;
+            experienceText.text = "max";
+            UpgradeLandButton.SetActive(true);
+        }
+    }
+}
